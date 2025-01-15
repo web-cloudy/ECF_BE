515c6fbeb99c6836a8d1c726d63fce7dc395047c
web-cloudy <nativedev124@gmail.com>
2025-01-15 14:39:03 -0800

    swagger issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { typeOrmConfig } from './config/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { StaffsModule } from './staffs/staffs.module';
 import { AuthModule } from './auth/auth.module';
-
+import { join } from 'path';
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'swagger-static'),
+    }),
     TypeOrmModule.forRoot(typeOrmConfig),
     UsersModule,
     StaffsModule,
